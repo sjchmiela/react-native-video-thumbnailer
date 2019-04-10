753659ff16eb2598d3f1e626a7215463223c517e
Stanisław Chmiela <sjchmiela@gmail.com>
2019-04-10 17:24:00 +0200

    Step 21: Set tolerance to some lower value to see differences between near times

--- ios/AJVideoThumbnailer.m
+++ ios/AJVideoThumbnailer.m
@@ -31,6 +31,8 @@ RCT_EXPORT_METHOD(generateThumbnailAsync:(NSString *)urlString options:(NSDictio
 
   AVAsset *asset = [AVAsset assetWithURL:sourceURL];
   AVAssetImageGenerator *imageGenerator = [[AVAssetImageGenerator alloc]initWithAsset:asset];
+  imageGenerator.requestedTimeToleranceAfter = CMTimeMake(1, 10);
+  imageGenerator.requestedTimeToleranceBefore = CMTimeMake(1, 10);
   if (options && [options[@"maximumSize"] isKindOfClass:[NSDictionary class]]) {
     NSNumber *width = [options[@"maximumSize"][@"width"] isKindOfClass:[NSNumber class]] ? options[@"maximumSize"][@"width"] : @(0);
     NSNumber *height = [options[@"maximumSize"][@"height"] isKindOfClass:[NSNumber class]] ? options[@"maximumSize"][@"height"] : @(0);
@@ -71,26 +73,6 @@ RCT_EXPORT_METHOD(generateThumbnailAsync:(NSString *)urlString options:(NSDictio
             @"height": @(thumbnail.size.height)
             });
 }
-
-// STEP 21
-// It looks like for similar values, generateThumbnailAsync returns exactly the same image.
-// (For example there's no noticable difference for timeMs === 4000 and timeMs === 5000, while the image
-// in the video changes.)
-//
-// This is due to the way in which AVAssetImageGenerator optimizes work it has to do. When we request for an image
-// the generator has to scan the video file and decode an image (which for some formats may take some time
-// due to the way the video file is compressed). To make this processing time smaller,
-// the generator by default may use a frame from different video time.
-//
-// Fortunately, we can configure the tolerance with imageGenerator.requestedTimeTolerance{Before,After}.
-// We can set them to kCMTimeZero (as the documentation suggests
-// https://developer.apple.com/documentation/avfoundation/avassetimagegenerator/1387751-requestedtimetoleranceafter?language=objc
-// ), to always generate exactly the frame the user requested us to, but then
-// a call with timeMs == 0 will fail to generate a thumbnail (at time 0 there's no frame yet, frames
-// start at timeMs == 1/framerate. To handle this case easily (but not fully) we can set the tolerance
-// to some reasonably small value, like one tenth of a second. To handle this fully we would probably either:
-// - check the video track of the asset and set the tolerance to (1/framerate)
-// - ensure that the requested time passed to AVAssetImageGenerator is (1/framerate) at minimum.
 
 // STEP 22
 // We can do even better than this! Notice how if you provide a negative value for the timeMs
